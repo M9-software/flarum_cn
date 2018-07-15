b26573b3124d014c4e6cde096a8447b6fd6d7098
radision <radision@126.com>
2018-07-15 15:37:06 +0800

    init

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'flarum',
+    'username' => 'root',
+    'password' => 'h2kgi_nhw3rvu5m',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+  ),
+  'url' => 'http://flarum.radision.biz',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
